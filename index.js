f1391111b8610e57533ee2e385e8ff1f84cc5b6d
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-03 20:53:06 +0300

    main file

---
+++ index.js
@@ -0,0 +1,15 @@
+const EventEmitter = require('./src/EventEmitter')
+const EventEmitterError = require('./src/EventEmitterError')
+const Event = require('./src/Event')
+const Factory = require('./src/Factory')
+const List = require('./src/List')
+
+module.exports = {
+  EventEmitter,
+  EventEmitterError,
+  Event,
+  Factory,
+  List
+}
+
+

791416a73566491d8ece321f6c4db947a2a3c69b
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-16 19:50:44 +0300

    Add List from @webkadiz/utils in entry point

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const EventEmitter = require('./src/EventEmitter')
 const EventEmitterError = require('./src/EventEmitterError')
 const Event = require('./src/Event')
 const Factory = require('./src/Factory')
-const List = require('./src/List')
+const List = require('@webkadiz/utils').List
 
 module.exports = {
   EventEmitter,
